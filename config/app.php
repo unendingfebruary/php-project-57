94f9d88c944c710f90e2f15beecb5da7fefc663a
alesmirnova <hardbitten96@gmail.com>
2023-01-12 16:56:36 +0100

    rollbar setup

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Rollbar\Laravel\RollbarServiceProvider::class,
 
         /*
          * Package Service Providers...
