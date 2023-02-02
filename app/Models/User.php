6482c793af8e2810fa6570b2f4127cc804e29abb
alesmirnova <hardbitten96@gmail.com>
2023-02-02 17:53:31 +0100

    fix phpstan errors

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property Carbon|null $email_verified_at
+ */
 class User extends Authenticatable
 {
     use HasApiTokens;
